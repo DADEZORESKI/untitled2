52a3d0bf881309d78033ae340553c85becc42550
DADEZORESKI <draganzoreski@gmail.com>
2023-02-02 19:19:13 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int dividend = 100;
+        int divisor = 5;
+        int quotient = dividend / divisor;
+        System.out.println(dividend + " / " + divisor + " = " + quotient);
+    }
+
+}
